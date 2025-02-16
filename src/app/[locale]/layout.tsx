2552f3caa28c4da2d0981cca2ec326d17d9df123
matheus <matheus.felipe.19rt@gmail.com>
2025-02-16 08:52:04 -0300

    feat(seo), fix: add locale-based metadata generation, set priority attribute to avatar image

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -9,20 +9,32 @@ import { notFound } from "next/navigation"
 
 const inter = Inter({ subsets: ["latin"] })
 
-export const metadata: Metadata = {
-	title: "Matheus Mortari",
-	description: "My Portfolio",
-	keywords: [
-		"Matheus Mortari",
-		"Matheus Mortari Portfolio",
-		"Matheus Mortari Developer",
-		"Matheus Mortari Full-Stack Developer",
-		"Developer Portfolio",
-		"Full-Stack Developer",
-		"Full-Stack Developer Portfolio"
-	],
-	other: {
-		"google-site-verification": "2j0bcfhh8FCYPpzFylzbiPjl3Pa0X7lMuG060ctsCsA"
+export async function generateMetadata({ params: { locale } }: { params: { locale: string } }): Promise<Metadata> {
+	const titles = {
+		en: "Matheus Mortari — Full-Stack Developer",
+		pt: "Matheus Mortari — Desenvolvedor Full-Stack"
+	}
+
+	const descriptions = {
+		en: "Welcome to my website, where you can learn about my work. Feel free to explore and connect!",
+		pt: "Bem-vindo ao meu site, onde você pode conhecer meu trabalho. Sinta-se à vontade para explorar e se conectar!"
+	}
+
+	return {
+		title: titles[locale],
+		description: descriptions[locale],
+		keywords: [
+			"Matheus Mortari",
+			"Matheus Mortari Portfolio",
+			"Matheus Mortari Developer",
+			"Matheus Mortari Full-Stack Developer",
+			"Developer Portfolio",
+			"Full-Stack Developer",
+			"Full-Stack Developer Portfolio"
+		],
+		other: {
+			"google-site-verification": "2j0bcfhh8FCYPpzFylzbiPjl3Pa0X7lMuG060ctsCsA"
+		}
 	}
 }
 
