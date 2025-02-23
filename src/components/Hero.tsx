78ac271fc50e8d091202e9907b68e22372edd412
matheus <matheus.felipe.19rt@gmail.com>
2025-02-23 10:23:35 -0300

    refactor(css): general CSS improvements and fixes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,28 +17,21 @@ export default function Hero() {
 			<div className="hero-background" />
 
 			<div className="hero-map scroll absolute inset-x-0 bottom-0 z-0 overflow-hidden opacity-20">
-				<Image src="/map.png" alt="Background" className="hidden size-full md:block" width={800} height={800} />
-				<Image src="/map.png" alt="Background" className="hidden size-full md:block" width={800} height={800} />
+				<Image src="/map.png" alt="" className="hidden size-full md:block" width={800} height={800} />
+				<Image src="/map.png" alt="" className="hidden size-full md:block" width={800} height={800} />
 			</div>
 
 			<div className="relative flex flex-col items-center gap-4 p-16">
-				<Image
-					src="/avatar.png"
-					alt="@matimortari"
-					priority={true}
-					width={180}
-					height={180}
-					className="float icon avatar"
-				/>
+				<Image src="/avatar.png" alt="Avatar" priority={true} width={150} height={150} className="float avatar" />
 
 				<div className="my-4 flex flex-col gap-4 text-center md:gap-2">
 					<h1 className={merriweather.className}>{title[locale]}</h1>
 					<h2 className={merriweather.className}>{subtitle[locale]}</h2>
 				</div>
 
-				<p className="max-w-sm text-center font-medium text-muted-foreground">{description[locale]}</p>
+				<p className="max-w-sm text-center text-sm text-muted-foreground md:text-base">{description[locale]}</p>
 
-				<div className="my-4 flex items-center justify-center gap-6 text-secondary">
+				<div className="my-4 flex flex-row items-center justify-center gap-6 text-secondary">
 					<Link href="https://github.com/matimortari" title="GitHub">
 						<Icon icon="simple-icons:github" width={25} height={25} className="scale" />
 					</Link>
