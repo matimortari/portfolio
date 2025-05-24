544a70e4bff822396bd5df81ce988e05e459b051
matheus <matheus.felipe.19rt@gmail.com>
2025-05-24 17:06:11 -0300

    Initial commit

---
+++ src/lib/i18n.ts
@@ -0,0 +1,29 @@
+import en from "./locales/en-US.json"
+import br from "./locales/pt-BR.json"
+
+export default defineI18nConfig(() => {
+  const config = useRuntimeConfig()
+
+  return {
+    legacy: false,
+    langDir: "./locales",
+    messages: { "en-US": en, "pt-BR": br },
+    baseUrl: config.public.baseUrl,
+    locales: [
+      {
+        code: "en",
+        iso: "en-US",
+        isCatchallLocale: true
+      },
+      {
+        code: "br",
+        iso: "pt-BR"
+      }
+    ],
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: "nuxt-lang",
+      redirectOn: "root"
+    }
+  }
+})
