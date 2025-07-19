80387244b7bd9c5e843b5aded030e0a1fee6ffc3
matheus <matheus.felipe.19rt@gmail.com>
2025-07-19 20:06:32 -0300

    style: minor code style tweaks

--- src/components/Stack.vue
+++ src/components/Stack.vue
@@ -1,14 +1,17 @@
 <template>
   <section id="stack" class="flex-1 text-center lg:text-start">
     <h3
-      v-motion :initial="{ opacity: 0, y: -20 }" :visible="{ opacity: 1, y: 0 }" :duration="400"
+      v-motion :initial="{ opacity: 0, y: -20 }"
+      :visible="{ opacity: 1, y: 0 }" :duration="400"
       class="font-serif my-10"
     >
       {{ t("index.stack.title") }}
     </h3>
 
     <div
-      v-motion :initial="{ opacity: 0, y: -40 }" :visible="{ opacity: 1, y: 0 }" :duration="400" :delay="200"
+      v-motion :initial="{ opacity: 0, y: -40 }"
+      :visible="{ opacity: 1, y: 0 }" :duration="400"
+      :delay="200"
       class="flex flex-col gap-2 lg:grid lg:grid-cols-3"
     >
       <UiStackCard
