b4f036f99a9d3fcc6d59c5b503a69e0b89d27fdb
matheus <matheus.felipe.19rt@gmail.com>
2025-07-20 23:00:15 -0300

    style: enable Tailwind CSS linting via eslint-plugin-tailwindcss

--- src/components/ui/StackCard.vue
+++ src/components/ui/StackCard.vue
@@ -2,7 +2,7 @@
   <div
     v-motion :initial="{ opacity: 0 }"
     :visible="{ opacity: 1 }" :duration="400"
-    class="card flex min-h-[145px] lg:min-h-[205px] xl:min-h-0 flex-col items-center justify-center text-center gap-2"
+    class="card flex min-h-[145px] flex-col items-center justify-center gap-2 text-center lg:min-h-[205px] xl:min-h-0"
   >
     <a :href="link" :title="link" target="_blank" rel="noopener noreferrer">
       <Icon :name="icon" size="25" class="animate-scale" />
@@ -12,7 +12,7 @@
       {{ name }}
     </h6>
 
-    <span class="flex-grow text-sm text-muted-foreground text-center lg:text-xs">
+    <span class="flex-grow text-center text-sm text-muted-foreground lg:text-xs">
       {{ description }}
     </span>
   </div>
