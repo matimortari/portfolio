57ff162483f77e796417678e03ff0c6b233bd8b2
Matheus Mortari <matheus.felipe.19rt@gmail.com>
2025-09-29 16:16:19 -0300

    chore(seo), docs: update brand assets, cleanup redundant head attributes; update README

--- app/app.vue
+++ app/app.vue
@@ -18,12 +18,16 @@ if (import.meta.client) {
 }
 
 useHead({
-  htmlAttrs: {
-    lang: locale.value,
-    dir: "ltr",
-  },
-  titleTemplate() {
-    return `${t("index.meta.title")}`
-  },
+  htmlAttrs: { lang: locale.value, dir: "ltr" },
+  link: [{ rel: "icon", href: "/favicon.svg" }],
+  meta: [
+    { name: "description", content: t("index.meta.description") },
+    { property: "og:title", content: ("index.meta.title") },
+    { property: "og:description", content: t("index.meta.description") },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { property: "og:type", content: "website" },
+    { property: "og:image", content: "https://matheus-mortari.vercel.app/og-image.png" },
+    { name: "google-site-verification", content: "2j0bcfhh8FCYPpzFylzbiPjl3Pa0X7lMuG060ctsCsA" },
+  ],
 })
 </script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -22,24 +22,7 @@ const { t } = useI18n()
 
 useHead({
   title: t("index.meta.title"),
-  link: [{ rel: "canonical", href: "https://matheus-mortari.vercel.app" }, { rel: "icon", href: "/favicon.svg" }],
-  meta: [
-    { name: "description", content: t("index.meta.description") },
-    { property: "og:title", content: ("index.meta.title") },
-    { property: "og:description", content: t("index.meta.description") },
-    { property: "og:type", content: "website" },
-    { property: "og:image", content: "https://matheus-mortari.vercel.app/og-image.png" },
-    { property: "og:url", content: "https://matheus-mortari.vercel.app" },
-    { name: "google-site-verification", content: "2j0bcfhh8FCYPpzFylzbiPjl3Pa0X7lMuG060ctsCsA" },
-  ],
-})
-
-useSeoMeta({
-  title: t("index.meta.title"),
-  description: t("index.meta.description"),
-  ogTitle: t("index.meta.title"),
-  ogDescription: t("index.meta.description"),
-  ogImage: "https://matheus-mortari.vercel.app/og-image.png",
-  ogUrl: "https://matheus-mortari.vercel.app",
+  link: [{ rel: "canonical", href: "https://matheus-mortari.vercel.app" }],
+  meta: [{ name: "description", content: t("index.meta.description") }],
 })
 </script>
